f199e91c1e4b4b3b43612c52de448b5e6c4d1823
ziyi zhang <ziyi.zhang2@wsu.edu>
2024-07-08 09:18:08 -0700

    add styles for header

---
+++ src/parts/header/Header.styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 20px;
+    background-color: #f9f9f9;
+    width: 100%;
+    margin-bottom: 10px;
+    overflow: hidden;
+    box-sizing: border-box;
+`;
+
+export const LogoIcon = styled.img`
+    width: auto;
+    max-width: 150px;
+    max-height: 75px;
+    padding-right: 1vw;
+`;
+
+export const PiqueIcon = styled.img`
+    width: auto;
+    max-width: 130px;
+    max-height: 130px;
+    padding-left: 1vw;
+`;
