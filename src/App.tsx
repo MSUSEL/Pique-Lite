543bbce103bd18e91e9660d5de94a07c12242ff3
ZjohnstonWSU <zachary.johnston@wsu.edu>
2024-11-24 18:09:51 -0800

    Fixed bug with page restructuring

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import MenuToggle from "./views/MenuToggle";
 const views: Record<string, JSX.Element> = {
   landing: <Landing />,
   overview: <Overview />,
-  projects: <Project />,
+  project: <Project />,
   //eval page set as overview for now while there is no eval page
   evaluate: <Overview />,
 };
