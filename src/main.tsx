ac29ffdea498983d796e9d1cf3f19635b154b479
Devjeet Roy <devjeetrr@gmail.com>
2024-11-30 12:50:01 -0800

    add jotai-devtools for dev mode.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import { Theme } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+
+import { DevTools } from "jotai-devtools";
+
+import "@radix-ui/themes/styles.css";
 import "./index.css";
 
+if (process.env.NODE_ENV === "development") {
+  import("jotai-devtools/styles.css");
+}
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Theme style={{ height: "100%", width: "100%" }}>
+  <Theme>
+    <StrictMode>
+      {process.env.NODE_ENV === "development" && <DevTools />}
       <App />
-    </Theme>
-  </StrictMode>
+    </StrictMode>
+  </Theme>
 );
