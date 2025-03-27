b63ba0b895aa70a8de65c744e07d482835f75349
Molly <molly_iverson@outlook.com>
2025-03-26 23:50:32 -0700

    Added routers for pagination logic in overview

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -1,25 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useSetAtom } from "jotai";
 import { State } from "../../state";
-import { Box, Flex, Heading, Link, Text } from "@radix-ui/themes";
+import { Box, Flex, Heading, Text } from "@radix-ui/themes";
 import { useProjects } from "../../composites/FileUploader/hooks/use-projects";
 import { ProjectCard } from "./ProjectCard";
 import Filters from "./Filters";
 import { getRisk } from "../../composites/RiskHelpers";
 import SearchBar from "../../composites/SearchBar";
 import { matchSorter } from "match-sorter";
+import { useSearchParams, Link, useNavigate } from "react-router-dom";
 
 const ITEMS_PER_PAGE = 5;
 
 const Overview: React.FC = () => {
   const setCurrentView = useSetAtom(State.currentView);
-  useEffect(() => {
-    setCurrentView("overview");
-  }, [setCurrentView]);
+  setCurrentView("overview");
 
   const { projects } = useProjects();
   const setProject = useSetAtom(State.selectedProject);
   const setVersion = useSetAtom(State.selectedVersion);
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const page = searchParams.get("page");
+  let currentPage = page ? parseInt(page, 10) : 1;
 
   if (!projects) return null;
 
@@ -31,7 +34,6 @@ const Overview: React.FC = () => {
     "Guarded",
     "Low",
   ]);
-  const [currentPage, setCurrentPage] = useState<number>(1);
   const [sliderValue, setSliderValue] = useState<number[]>([0, 1.0]);
   const [searchQuery, setSearchQuery] = useState<string>("");
 
@@ -67,6 +69,7 @@ const Overview: React.FC = () => {
 
   //Pagination logic
   const totalPages = Math.ceil(searchFilteredProjects.length / ITEMS_PER_PAGE);
+  currentPage = currentPage > totalPages ? 1 : currentPage;
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
   const projectsToDisplay = searchFilteredProjects.slice(startIndex, endIndex);
@@ -111,7 +114,6 @@ const Overview: React.FC = () => {
           <PaginationButtons
             currentPage={currentPage}
             totalPages={totalPages}
-            setCurrentPage={setCurrentPage}
           />
         )}
         {/* Display paginated projects */}
@@ -124,6 +126,7 @@ const Overview: React.FC = () => {
               setCurrentView("project");
               setProject(uuid);
               setVersion(versionIndex);
+              navigate(`/projectview?projectid=${uuid}&versionid=${versionIndex}`);
             }}
           />
         ))}
@@ -131,7 +134,6 @@ const Overview: React.FC = () => {
           <PaginationButtons
             currentPage={currentPage}
             totalPages={totalPages}
-            setCurrentPage={setCurrentPage}
           />
         )}
       </Flex>
@@ -142,7 +144,6 @@ const Overview: React.FC = () => {
 interface PaginationButtonsProps {
   currentPage: number;
   totalPages: number;
-  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
 //Pagination logic
@@ -150,24 +151,14 @@ interface PaginationButtonsProps {
 export const PaginationButtons: React.FC<PaginationButtonsProps> = ({
   currentPage,
   totalPages,
-  setCurrentPage,
 }) => {
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const handlePreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
   return (
     <Flex direction="row" gap="3" mt="4">
       <Link
-        onClick={handlePreviousPage}
+        to={`?page=${currentPage - 1}`}
+        onClick={(e) => {
+          if (currentPage === 1) e.preventDefault();
+        }}
         style={{
           color: currentPage === 1 ? "gray" : "blue",
           cursor: currentPage === 1 ? "not-allowed" : "pointer",
@@ -178,7 +169,7 @@ export const PaginationButtons: React.FC<PaginationButtonsProps> = ({
       {Array.from({ length: totalPages }, (_, index) => (
         <Link
           key={index + 1}
-          onClick={() => setCurrentPage(index + 1)}
+          to={`?page=${index + 1}`}
           style={{
             color: currentPage === index + 1 ? "black" : "blue",
             cursor: "pointer",
@@ -188,10 +179,13 @@ export const PaginationButtons: React.FC<PaginationButtonsProps> = ({
         </Link>
       ))}
       <Link
-        onClick={handleNextPage}
+        to={`?page=${currentPage + 1}`}
         style={{
           color: currentPage === totalPages ? "gray" : "blue",
           cursor: currentPage === totalPages ? "not-allowed" : "pointer",
+        }}
+        onClick={(e) => {
+          if (currentPage === totalPages) e.preventDefault();
         }}
       >
         Next
