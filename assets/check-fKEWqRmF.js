99fc9f8745964c72d53610f45969e4eef39c43d6
Ziyi Zhang <zhangziyi@Mac-mini.local>
2025-12-15 21:59:14 -0800

    Deploy (new code, old index style)

--- assets/check-fKEWqRmF.js
+++
@@ -1,6 +0,0 @@
-import{i as c}from"./button-R8BdzlHL.js";/**
- * @license lucide-react v0.508.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
