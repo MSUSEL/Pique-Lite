76c7c04f73d3a652167b06e831ac75946b41cbba
Ziyi Zhang <zhangziyi@Mac-mini.local>
2025-12-15 20:09:44 -0800

    Deploy (SPA entry for GitHub Pages)

---
+++ assets/use-projects-WE21vitr.js
@@ -0,0 +1,1 @@
+import{u,S as r,j}from"./data-ClmSUR48.js";const m=()=>{const s=u(r.projects),o=j(r.projects);return{projects:s,addProject:t=>{o((e={})=>({...e,[t.uuid]:t}))},removeProject:t=>{o(e=>e?Object.fromEntries(Object.entries(e).filter(([c])=>c!==t)):void 0)}}};export{m as u};
